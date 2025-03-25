59134c38e84c91de1f572c013f5f0fb07b1abdc8
trongnguyen-npt <npt2672001@gmail.com>
2025-03-25 09:44:57 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFound from "@/views/NotFound.vue"; // Import thêm NotFound component
 
 const routes = [
   {
@@ -7,11 +8,16 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
+  {
+    path: "/:pathMatch(.*)*", // Định nghĩa route khớp với tất cả các URL
+    name: "notfound",
+    component: NotFound, // Trỏ đến component NotFound
+  },
 ];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
-
 
 export default router;
